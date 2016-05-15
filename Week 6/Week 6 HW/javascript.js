605292525ade21fe85bf6a844883eed8fb90259b
randynj <rsingh1415@gmail.com>
2016-05-15 18:11:06 -0400

    Week 6 HW
    more code comments

--- Week 6/Week 6 HW/javascript.js
+++ Week 6/Week 6 HW/javascript.js
@@ -1,3 +1,4 @@
+//this block of code executes when user clicks on the 1st "Read More" under the 1st image
 $(document).ready(function(){
     $("p#read-more-one").click(function(){
         $("p#hidden-text-one").toggle("slow");
@@ -6,7 +7,7 @@ $(document).ready(function(){
     });
 });
 
-
+//this block of code executes when user clicks on the 2nd "Read More" under the 2nd image
 $(document).ready(function(){
     $("p#read-more-two").click(function(){
         $("p#hidden-text-two").toggle("slow");
@@ -15,7 +16,7 @@ $(document).ready(function(){
     });
 });
 
-
+//this block of code executes when user click "Learn More" in asides-two div
 $(document).ready(function() {
 	$(".learnmore").click(function() {
 		$("#learnmoretext").slideDown("slow");
@@ -23,7 +24,7 @@ $(document).ready(function() {
 	})
 })
 
-
+//this block of code prevents the "Contact Us" link in header from re-directing to the outside URL
 $(document).ready(function() {
 	$(".event-preventDefault").click(function(event) {
 		event.preventDefault();
