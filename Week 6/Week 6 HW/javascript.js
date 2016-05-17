d83e1562e5607687186eae2d411864f3e66140a4
randynj <rsingh1415@gmail.com>
2016-05-17 14:35:25 -0400

    Week 6 HW Final Draft

--- Week 6/Week 6 HW/javascript.js
+++ Week 6/Week 6 HW/javascript.js
@@ -1,4 +1,3 @@
-//this block of code executes when user clicks on the 1st "Read More" under the 1st image
 $(document).ready(function(){
     $("p#read-more-one").click(function(){
         $("p#hidden-text-one").toggle("slow");
@@ -7,7 +6,7 @@ $(document).ready(function(){
     });
 });
 
-//this block of code executes when user clicks on the 2nd "Read More" under the 2nd image
+
 $(document).ready(function(){
     $("p#read-more-two").click(function(){
         $("p#hidden-text-two").toggle("slow");
@@ -16,7 +15,7 @@ $(document).ready(function(){
     });
 });
 
-//this block of code executes when user click "Learn More" in asides-two div
+
 $(document).ready(function() {
 	$(".learnmore").click(function() {
 		$("#learnmoretext").slideDown("slow");
@@ -24,7 +23,7 @@ $(document).ready(function() {
 	})
 })
 
-//this block of code prevents the "Contact Us" link in header from re-directing to the outside URL
+
 $(document).ready(function() {
 	$(".event-preventDefault").click(function(event) {
 		event.preventDefault();
